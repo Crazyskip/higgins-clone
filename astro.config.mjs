3d1c818f83def9232d8f2a94381cdddf4996b121
crazyskip <crazyskip73@gmail.com>
2022-12-06 16:24:21 +1000

    Complete Portal Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,5 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()]
+  integrations: [tailwind(), react(), image()],
 });
